a0534630181c362e579e08a593351c7a55ffca3b
misuo-mada <hokuto.3628@gmail.com>
2025-06-07 00:40:48 +0900

    最初のコミットメッセージ

--- server.cjs
+++ server.cjs
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // ← 修正ポイント！
 app.use(express.static(path.join(__dirname, 'public')));
 
 let imageHistory = [];
